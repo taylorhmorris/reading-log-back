00ffb7f4b2289b18b1f5ace870974c8a27d99ebd
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-07-28 18:47:37 +0200

    feat: install ConfigModule to handle .env files
    - install @nestjs/config
    - import global ConfigModule to read from .env and .env.default
    - create .env.default with PORT variable
    - update .gitignore to ignore .env file to prevent accidental pushes
    - update main.ts to listen on PORT instead of 3000
    - update CHANGELOG.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      envFilePath: ['.env.development', '.env'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
