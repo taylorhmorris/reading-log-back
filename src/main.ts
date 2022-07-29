0043a3ad06453ab76d818e7eef795c003fa40a42
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-07-29 18:51:16 +0200

    build: install swagger to document api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Reading Log')
+    .setDescription('Reading Log API')
+    .setVersion('0.0.1')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   const configService = app.get(ConfigService);
+  
   const port = configService.get('PORT');
   await app.listen(port);
 }
