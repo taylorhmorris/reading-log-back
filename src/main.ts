2eda87855bf0b4c8800ec968b64ad404c72b8329
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-07-30 11:35:55 +0200

    feat: implement ClassSerializer
    - transform server response

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
@@ -25,6 +25,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const port = configService.get('PORT');
   await app.listen(port);
