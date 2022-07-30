4ee90af35a80e538e736cee86adf2b7a4f8dad83
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-07-30 12:30:20 +0200

    test: fix user tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -8,7 +8,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useValue: jest.fn() }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
