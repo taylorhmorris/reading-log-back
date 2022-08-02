ae8e9082605298ebdd5895da54019d041af1a33e
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-08-02 19:16:53 +0200

    feat: add email column to User

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,4 @@
-import { PartialType, PickType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PickType(PartialType(CreateUserDto), [
-  'username',
-  'password',
-]) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
