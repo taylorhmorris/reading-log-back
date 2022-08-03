704327ddbd3fcbe811616a02b054991c8773d75c
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-08-03 19:40:07 +0200

    refactor: combine Owned Dto and Entity
    re #19

--- src/common/entities/owned.entity.ts
+++ src/common/entities/owned.entity.ts
@@ -1,10 +1,21 @@
 import { User } from '@/users/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsPositive } from 'class-validator';
 import { ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 export class OwnedEntity {
+  @ApiProperty({
+    description: "The Entity's id",
+  })
+  @IsPositive()
+  @IsInt()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: "The Entity's owner",
+  })
+  @IsNotEmpty()
   @ManyToOne(() => User, { onDelete: 'CASCADE' })
   owner: User;
 }
