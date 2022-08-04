373f2b6497757d1b04ee5a6d66735d8bb0e84cd2
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-08-04 12:26:35 +0200

    fix: update User to create with password
    Fix #36

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,14 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Column } from 'typeorm';
 import { User } from '../entities/user.entity';
 
-export class CreateUserDto extends PickType(User, [
-  'username',
-  'email',
-  'password',
-]) {}
+export class CreateUserDto extends PickType(User, ['username', 'email']) {
+  @ApiProperty({
+    description: "The user's password",
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  password: string;
+}
