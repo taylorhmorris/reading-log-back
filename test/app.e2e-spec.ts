58158ad9f5c4eeded2177f4769407d80eb1cc2e2
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-08-07 00:17:11 +0200

    test: make e2e tests run

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { ClassSerializerInterceptor, INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reflector } from '@nestjs/core';
+import { Connection } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ConfigModule.forRoot({
+          cache: true,
+          envFilePath: ['.env.test', '.env.development', '.env'],
+          isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USER'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_NAME'),
+            autoLoadEntities: true,
+            synchronize: configService.get('DB_SYNC'),
+            dropSchema: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: ['1'],
+    });
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404)
   });
 });
