5011c7b372817e40faaa26672cd8b632d36019eb
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-08-07 16:39:28 +0200

    test: create prepareTestingApp.ts
    - splits out app creation for e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,58 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ClassSerializerInterceptor, INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Reflector } from '@nestjs/core';
-import { Connection } from 'typeorm';
+import { prepareTestingApp } from './prepareTestingApp';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          cache: true,
-          envFilePath: ['.env.test', '.env.development', '.env'],
-          isGlobal: true,
-        }),
-        TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          useFactory: (configService: ConfigService) => ({
-            type: 'postgres',
-            host: configService.get('DB_HOST'),
-            port: configService.get('DB_PORT'),
-            username: configService.get('DB_USER'),
-            password: configService.get('DB_PASSWORD'),
-            database: configService.get('DB_NAME'),
-            autoLoadEntities: true,
-            synchronize: configService.get('DB_SYNC'),
-            dropSchema: true,
-          }),
-          inject: [ConfigService],
-        }),
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-
-    app.enableVersioning({
-      type: VersioningType.URI,
-      defaultVersion: ['1'],
-    });
-
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
-
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
+    app = await prepareTestingApp();
     await app.init();
   });
 
@@ -61,8 +15,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(404)
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
