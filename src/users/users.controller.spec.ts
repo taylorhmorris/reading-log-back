c4334fd1f2b4d79614ed24e7ea3e5ee67b163093
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-10-12 15:49:13 +0200

    test: add users.controller.update test
    to make sure tests can be done with casl module mocked

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,13 +2,14 @@ import { CaslAbilityFactory } from '@/casl/casl-ability.factory';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,16 +22,25 @@ describe('UsersController', () => {
       ],
       controllers: [UsersController],
       providers: [
-        { provide: UsersService, useValue: jest.fn() },
+        { provide: UsersService, useClass: UsersService },
         { provide: CaslAbilityFactory, useValue: jest.fn() },
         { provide: getRepositoryToken(User), useClass: Repository<User> },
       ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('update', () => {
+    it('should allow users to modify their own username', () => {
+      jest.spyOn(usersService, 'update').mockResolvedValue(new UpdateResult());
+      const result = controller.update(1, { username: 'newName' });
+      expect(result).resolves.toBeInstanceOf(UpdateResult);
+    });
   });
 });
