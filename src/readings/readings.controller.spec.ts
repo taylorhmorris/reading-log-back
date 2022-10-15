9e4797787134be8879baa2609f66b1392b81e6b6
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-10-15 22:03:04 +0200

    feat(casl): guard readings.controller
    - delete and update

--- src/readings/readings.controller.spec.ts
+++ src/readings/readings.controller.spec.ts
@@ -1,4 +1,10 @@
+import { CaslModule } from '@/casl/casl.module';
+import { User } from '@/users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Reading } from './entities/reading.entity';
 import { ReadingsController } from './readings.controller';
 import { ReadingsService } from './readings.service';
 
@@ -7,8 +13,20 @@ describe('ReadingsController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          cache: true,
+          envFilePath: ['.env.test'],
+          isGlobal: true,
+        }),
+        CaslModule,
+      ],
       controllers: [ReadingsController],
-      providers: [{ provide: ReadingsService, useValue: jest.fn() }],
+      providers: [
+        { provide: ReadingsService, useValue: jest.fn() },
+        { provide: getRepositoryToken(User), useClass: Repository<User> },
+        { provide: getRepositoryToken(Reading), useClass: Repository<Reading> },
+      ],
     }).compile();
 
     controller = module.get<ReadingsController>(ReadingsController);
